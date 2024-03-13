1ef28cb04ae3e8ec94446b51e9fd53a8c22451ff
양원준 <anyas1398@gmail.com>
2024-03-13 09:29:13 +0900

    .

---
+++ 2024-03-13/2024-03-13.js
@@ -0,0 +1,19 @@
+function getUseerInfo() {
+    // 가정: user 객체에서 정보를 가져온다
+    var user = {
+        name: 'Alice',
+        age: 25
+    };
+    return [user.name, user.age];
+}
+var _a = getUseerInfo(), userName = _a[0], userAge = _a[1];
+console.log("\uC0AC\uC6A9\uC790 \uC774\uB984: ".concat(userName), "\uC0AC\uC6A9\uC790 \uB098\uC774: ".concat(userAge));
+var dan = [];
+for (var i = 1; i <= 10; i++) {
+    dan.push(i * 2);
+}
+console.log(dan);
+function swap(arg) {
+    return [arg[1], arg[0]];
+}
+console.log(swap([5, 10]));
---
+++ 2024-03-13/2024-03-13.ts
@@ -0,0 +1,23 @@
+function getUseerInfo(): [string, number] {
+    // 가정: user 객체에서 정보를 가져온다
+    const user = {
+        name: 'Alice',
+        age: 25
+    };
+    return [user.name, user.age];
+}
+
+const [userName, userAge] = getUseerInfo();
+console.log(`사용자 이름: ${userName}`, `사용자 나이: ${userAge}`);
+
+const dan: number[] = [];
+
+for (let i = 1; i <= 10; i++) {
+    dan.push(i * 2);
+}
+console.log(dan);
+
+function swap(arg: [number, number]): [number, number] {
+    return [arg[1], arg[0]];
+}
+console.log(swap([5, 10]));
