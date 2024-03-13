3b79032a728a5f5fc3579843bfed8225b0a2dafd
양원준 <anyas1398@gmail.com>
2024-03-13 09:02:05 +0900

    .

---
+++ test.js
@@ -0,0 +1,25 @@
+// 2024-03-12
+var msg = 'Hello';
+var num = 123;
+var bool = true;
+function add(num1, num2) {
+    return num1 + num2;
+}
+;
+var plus = function (num1, num2) { return num1 + num2; };
+var age = 30;
+var aaa = "Johb-m";
+console.log(aaa.toUpperCase());
+var nameObj = new String("John");
+function max(num1, num2) {
+    if (num1 > num2)
+        return num1;
+    return num2;
+}
+;
+console.log(max(1, 2));
+var arr = [10, 20, 30];
+arr.forEach(function (a) {
+    console.log(a);
+    return a;
+});
---
+++ test.ts
@@ -0,0 +1,43 @@
+// 2024-03-12
+
+const msg: string = 'Hello';
+const num: number = 123;
+const bool: boolean = true;
+
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+};
+
+const plus = (num1: number, num2: number): number => num1 + num2;
+
+let age: number = 30;
+let aaa: string = "Johb-m";
+console.log(aaa.toUpperCase());
+
+let nameObj = new String("John");
+
+function max(num1: number, num2: number): number {
+    if (num1 > num2)
+        return num1;
+    return num2;
+};
+
+console.log(max(1, 2));
+
+const arr = [10, 20, 30];
+arr.forEach((a: number): number => {
+    console.log(a);
+    return a;
+});
+
+// Array<string>
+// string[]
+
+var companies: Array<string> = ['네이버', '삼성', 'LG'];
+var companies: string[] = ['네이버', '삼성', 'LG'];
+var cards: number[] = [1, 2, 3, 4];
+
+const arr1: [number] = [1];
+arr1.push(2);
+console.log(arr1);
+
