8e9251ffd4bea2e8bfbfc66ff0675a06578bd883
양원준 <anyas1398@gmail.com>
2024-03-28 09:06:37 +0900

    .

--- 2024-03-27/test.js
+++ 2024-03-27/test.js
@@ -1,38 +1,120 @@
-function fetchbody() {
-    var patient = [
+// interface Patient {
+//     name: string;
+//     head: { [key: string]: { symp: string } };
+//     body: { [key: string]: { symp: string } };
+// }
+function createSchedule() {
+    var schedule = [
         {
-            name: "양원준",
-            head: {
-                "eye": { symp: "충혈" },
-                "nose": { symp: "막힘" }
+            monday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "휴식",
+                seventh: "저녁",
+                eighth: "휴식",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
             },
-            body: {
-                "stomach": { symp: "매스꺼움" }
-            }
-        },
-        {
-            name: "박준범",
-            head: {
-                "mental": { symp: "이상함" }
+            tuesday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "운동",
+                seventh: "저녁",
+                eighth: "독서",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
             },
-            body: {
-                "ankle": { symp: "병신" }
+            wednesday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "등산",
+                seventh: "저녁",
+                eighth: "영화 보기",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            thursday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "댄스 수업",
+                seventh: "저녁",
+                eighth: "음악 감상",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            friday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "그림 그리기",
+                seventh: "저녁",
+                eighth: "게임",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            saturday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "운동",
+                fourth: "점심",
+                fifth: "장보기",
+                sixth: "청소",
+                seventh: "저녁",
+                eighth: "친구 만나기",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            sunday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "휴식",
+                fourth: "점심",
+                fifth: "가족과 함께 시간 보내기",
+                sixth: "책 읽기",
+                seventh: "저녁",
+                eighth: "다음 주 계획 세우기",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
             }
         }
     ];
-    return patient;
+    return schedule;
 }
-function stringifyPatient(patient) {
-    var result = "".concat(patient.name, ":\n");
-    result += "  head:\n";
-    for (var key in patient.head) {
-        result += "    ".concat(key, ": ").concat(JSON.stringify(patient.head[key]), "\n");
-    }
-    result += "  body:\n";
-    for (var key in patient.body) {
-        result += "    ".concat(key, ": ").concat(JSON.stringify(patient.body[key]), "\n");
+function stringifySchedule(schedule) {
+    var result = "";
+    for (var key in schedule) {
+        if (key in schedule) {
+            result += "".concat(key, ":\n");
+            var daySchedule = schedule[key];
+            for (var subKey in daySchedule) {
+                var value = daySchedule[subKey];
+                var strValue = typeof value === 'string' ? value : JSON.stringify(value);
+                result += "  ".concat(subKey, ": ").concat(strValue, "\n");
+            }
+        }
     }
     return result;
 }
-var list = fetchbody();
-list.forEach(function (patient) { return console.log(stringifyPatient(patient)); });
+var schedules = createSchedule();
+schedules.forEach(function (schedule) { return console.log(stringifySchedule(schedule)); });
--- 2024-03-27/test.ts
+++ 2024-03-27/test.ts
@@ -1,47 +1,192 @@
-interface Patient {
-    name: string;
-    head: { [key: string]: { symp: string } };
-    body: { [key: string]: { symp: string } };
+// interface Patient {
+//     name: string;
+//     head: { [key: string]: { symp: string } };
+//     body: { [key: string]: { symp: string } };
+// }
+
+// function fetchbody(): Patient[] {
+//     const patient: Patient[] = [
+//         {
+//             name: "양원준",
+//             head: {
+//                 "eye": { symp: "충혈" },
+//                 "nose": { symp: "막힘" }
+//             },
+//             body: {
+//                 "stomach": { symp: "매스꺼움" }
+//             }
+//         },
+//         {
+//             name: "박준범",
+//             head: {
+//                 "mental": { symp: "이상함" }
+//             },
+//             body: {
+//                 "ankle": { symp: "병신" }
+//             }
+//         }
+//     ];
+
+//     return patient;
+// }
+
+// function stringifyPatient(patient: Patient): string {
+//     let result = `${patient.name}:\n`;
+//     result += "  head:\n";
+//     for (const key in patient.head) {
+//         result += `    ${key}: ${JSON.stringify(patient.head[key])}\n`;
+//     }
+//     result += "  body:\n";
+//     for (const key in patient.body) {
+//         result += `    ${key}: ${JSON.stringify(patient.body[key])}\n`;
+//     }
+//     return result;
+// }
+
+// const list: Patient[] = fetchbody();
+// list.forEach(patient => console.log(stringifyPatient(patient)));
+
+
+interface Schedule {
+    monday: Day_Schedule;
+    tuesday: Day_Schedule;
+    wednesday: Day_Schedule;
+    thursday: Day_Schedule;
+    friday: Day_Schedule;
+    saturday: Day_Schedule;
+    sunday: Day_Schedule;
 }
 
-function fetchbody(): Patient[] {
-    const patient: Patient[] = [
+interface Day_Schedule {
+    first: string;
+    second: string;
+    third: string;
+    fourth: string;
+    fifth: string;
+    sixth: string;
+    seventh: string;
+    eighth: string;
+    ninth: string;
+    tenth: string;
+    eleventh: string;
+}
+
+function createSchedule(): Schedule[] {
+    const schedule: Schedule[] = [
         {
-            name: "양원준",
-            head: {
-                "eye": { symp: "충혈" },
-                "nose": { symp: "막힘" }
+            monday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "휴식",
+                seventh: "저녁",
+                eighth: "휴식",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
             },
-            body: {
-                "stomach": { symp: "매스꺼움" }
-            }
-        },
-        {
-            name: "박준범",
-            head:{ 
-                "mental": { symp: "이상함"}
+            tuesday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "운동",
+                seventh: "저녁",
+                eighth: "독서",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
             },
-            body: {
-                "ankle": { symp: "병신"}
+            wednesday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "등산",
+                seventh: "저녁",
+                eighth: "영화 보기",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            thursday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "댄스 수업",
+                seventh: "저녁",
+                eighth: "음악 감상",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            friday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "출근",
+                fourth: "점심",
+                fifth: "출근",
+                sixth: "그림 그리기",
+                seventh: "저녁",
+                eighth: "게임",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            saturday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "운동",
+                fourth: "점심",
+                fifth: "장보기",
+                sixth: "청소",
+                seventh: "저녁",
+                eighth: "친구 만나기",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
+            },
+            sunday: {
+                first: "일어나기",
+                second: "아침 식사",
+                third: "휴식",
+                fourth: "점심",
+                fifth: "가족과 함께 시간 보내기",
+                sixth: "책 읽기",
+                seventh: "저녁",
+                eighth: "다음 주 계획 세우기",
+                ninth: "잠",
+                tenth: "",
+                eleventh: ""
             }
         }
     ];
 
-    return patient;
+    return schedule;
 }
 
-function stringifyPatient(patient: Patient): string {
-    let result = `${patient.name}:\n`;
-    result += "  head:\n";
-    for (const key in patient.head) {
-        result += `    ${key}: ${JSON.stringify(patient.head[key])}\n`;
-    }
-    result += "  body:\n";
-    for (const key in patient.body) {
-        result += `    ${key}: ${JSON.stringify(patient.body[key])}\n`;
+function stringifySchedule(schedule: Schedule): string {
+    let result = "";
+    for (const key in schedule) {
+        if (key in schedule) {
+            result += `${key}:\n`;
+            const daySchedule = schedule[key as keyof Schedule];
+            for (const subKey in daySchedule) {
+                const value = daySchedule[subKey as keyof Day_Schedule];
+                const strValue = typeof value === 'string' ? value : JSON.stringify(value);
+                result += `  ${subKey}: ${strValue}\n`;
+            }
+        }
     }
     return result;
 }
 
-const list: Patient[] = fetchbody();
-list.forEach(patient => console.log(stringifyPatient(patient)));
+const schedules: Schedule[] = createSchedule();
+schedules.forEach(schedule => console.log(stringifySchedule(schedule)));
+
