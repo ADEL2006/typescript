979e615256e92c9a3305e3a514fba57f07fd1a98
양원준 <anyas1398@gmail.com>
2024-04-03 10:30:57 +0900

    Update App.tsx

--- 2024-04-02/src/App.tsx
+++ 2024-04-02/src/App.tsx
@@ -1,13 +1,30 @@
 import * as D from './data';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <div>
-      <p>
-        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
-      </p>
-      <img src={D.randomAvatar()} height="50" />
-      <img src={D.randomImage()} height="300" />
-    </div>
-  );
-}
+// export default function App() {
+//   return (
+//     <div>
+//       <p>
+//         {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+//       </p>
+//       <img src={D.randomAvatar()} height="50" />
+//       <img src={D.randomImage()} height="300" />
+//     </div>
+//   );
+// }
+
+// ------------------------------------------------------------------------------------------------
+
+ export default class App extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to google</p>
+          </a>
+        </li>
+      </ul>
+    )
+  }
+ }
