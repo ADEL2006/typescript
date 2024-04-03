d7b758ea408ef2a89f95d177ec60cd7bc83ad9f7
양원준 <anyas1398@gmail.com>
2024-04-03 10:12:52 +0900

    Update index.tsx

--- 2024-04-02/src/index.tsx
+++ 2024-04-02/src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-
+import * as D from './data';
 
 // const children = [
 //   <li>
@@ -23,20 +23,38 @@ import ReactDOM from 'react-dom/client';
 // const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 // root.render(rootVirtualDOM);
 
-const websites = [
-  { name: "Google", url: "http://www.google.com" },
-  { name: "Facebook", url: "http://www.facebook.com" },
-  { name: "Twitter", url: "http://twitter.com" }
-];
-const children = websites.map((site, index) => (
-  <li key={index}>
-    <a href={site.url} target="_blank" rel="noreferrer noopener">
-      <p>go to {site.name}</p>
-    </a>
-  </li>
-));
-// 생성된 li 요소 배열을 ul 요소에 포함
-const rootVirtualDOM = <ul>{children}</ul>;
-// ReactDOM.createRoot를 사용하여 DOM에 렌더링
+// ------------------------------------------------------------------------------------------------
+
+// const websites = [
+//   { name: "Google", url: "http://www.google.com" },
+//   { name: "Facebook", url: "http://www.facebook.com" },
+//   { name: "Twitter", url: "http://twitter.com" }
+// ];
+// const children = websites.map((site, index) => (
+//   <li key={index}>
+//     <a href={site.url} target="_blank" rel="noreferrer noopener">
+//       <p>go to {site.name}</p>
+//     </a>
+//   </li>
+// ));
+// // 생성된 li 요소 배열을 ul 요소에 포함
+// const rootVirtualDOM = <ul>{children}</ul>;
+// // ReactDOM.createRoot를 사용하여 DOM에 렌더링
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(rootVirtualDOM);
+
+// ------------------------------------------------------------------------------------------------
+
+const children = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src={D.randomAvatar()} width={100} height={100} />
+  </div>
+))
+const rootVirtualDOM = <div>{children}</div>;
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(rootVirtualDOM);
