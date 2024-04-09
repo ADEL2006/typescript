6ef21efeb64b0cf3bd071396df61feea60b609da
양원준 <anyas1398@gmail.com>
2024-04-09 10:21:25 +0900

    .

--- 2024-04-02/src/App.tsx
+++ 2024-04-02/src/App.tsx
@@ -2,6 +2,17 @@ import * as D from './data';
 import P from './P';
 import { Component } from 'react';
 import ClassComponent from './ClassComponent';
+import EventListener from './pages/EventListener';
+import OnClick from './pages/OnClick';
+import ReactOnClick from './pages/ReactOnClick';
+import DispatchEvent from './pages/DispatchEvent';
+import EventBubbling from './pages/EventBubbling';
+import StopPropagation from './pages/StopPropagation';
+import VariousInputs from './pages/VariousInputs';
+import OnChange from './pages/OnChange';
+import FileInput from './pages/FileInput';
+import DragDrop from './pages/DragDrop';
+import FileDrop from './pages/FileDrop';
 
 // export default function App() {
 //   return (
@@ -111,7 +122,27 @@ import ClassComponent from './ClassComponent';
 
 // ------------------------------------------------------------------------------------------------
 
+// export default function App() {
+//   const texts = ['hello', 'world']. map((text, index) => <P key={index} children={text}/>)
+//   return <div children={texts}/>
+// }
+
+// ------------------------------------------------------------------------------------------------
+
 export default function App() {
-  const texts = ['hello', 'world']. map((text, index) => <P key={index} children={text}/>)
-  return <div children={texts}/>
+  return (
+    <div>
+      <FileDrop />
+      <DragDrop />
+      <FileInput />
+      <OnChange />
+      <VariousInputs />
+      <StopPropagation />
+      <EventBubbling />
+      <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener />
+    </div>
+  )
 }
