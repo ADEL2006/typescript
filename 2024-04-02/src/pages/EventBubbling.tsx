bdc5e47b92be3cbf76e38fad63b379d1d83a255d
양원준 <anyas1398@gmail.com>
2024-04-09 11:30:57 +0900

    Update EventBubbling.tsx

--- 2024-04-02/src/pages/EventBubbling.tsx
+++ 2024-04-02/src/pages/EventBubbling.tsx
@@ -1,3 +1,18 @@
-export default function CopyMe() {
-    return <div>CopyMe</div>
+import type {SyntheticEvent} from 'react';
+
+export default function EventBubbling(){
+    const onDivClick = (e: SyntheticEvent) => {
+        const {isTrusted, target, bubbles, currentTarget} = e;
+        console.log('click event bubble on <div>', isTrusted, target, bubbles, currentTarget);
+    }
+    const onButtonClick = (e: SyntheticEvent) => {
+        const {isTrusted, target, bubbles, currentTarget} = e;
+        console.log('click event starts at <button>', isTrusted, target,bubbles)
+    }
+    return(
+        <div>
+            <p>EventBubbling</p>
+            <button onClick={onButtonClick}>Click Me</button>
+        </div>
+    )
 }
