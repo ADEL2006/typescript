39f1b7383e97aef07f02635412970f0c16e2d02c
양원준 <anyas1398@gmail.com>
2024-04-09 11:31:06 +0900

    Update EventBubbling.tsx

--- 2024-04-02/src/pages/EventBubbling.tsx
+++ 2024-04-02/src/pages/EventBubbling.tsx
@@ -1,15 +1,15 @@
-import type {SyntheticEvent} from 'react';
+import type { SyntheticEvent } from 'react';
 
-export default function EventBubbling(){
+export default function EventBubbling() {
     const onDivClick = (e: SyntheticEvent) => {
-        const {isTrusted, target, bubbles, currentTarget} = e;
+        const { isTrusted, target, bubbles, currentTarget } = e;
         console.log('click event bubble on <div>', isTrusted, target, bubbles, currentTarget);
     }
     const onButtonClick = (e: SyntheticEvent) => {
-        const {isTrusted, target, bubbles, currentTarget} = e;
-        console.log('click event starts at <button>', isTrusted, target,bubbles)
+        const { isTrusted, target, bubbles, currentTarget } = e;
+        console.log('click event starts at <button>', isTrusted, target, bubbles)
     }
-    return(
+    return (
         <div>
             <p>EventBubbling</p>
             <button onClick={onButtonClick}>Click Me</button>
