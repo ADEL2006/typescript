e5fd36215833668dc98b71d763faa890a34defb1
양원준 <anyas1398@gmail.com>
2024-04-16 10:28:04 +0900

    Update OnChange.tsx

--- 2024-04-02/src/pages/OnChange.tsx
+++ 2024-04-02/src/pages/OnChange.tsx
@@ -1,17 +1,42 @@
+import type { ChangeEvent } from "react";
+
 export default function DispatchEvent() {
-    const onCallDispatchEvent = () => {
-        console.log('onCallDispatchEvent')
-        document.getElementById('root')?.dispatchEvent(new Event('click', { bubbles: true }));
+    // const onCallDispatchEvent = () => {
+    //     console.log('onCallDispatchEvent')
+    //     document.getElementById('root')?.dispatchEvent(new Event('click', { bubbles: true }));
+    // }
+    // const onCallClick = () => {
+    //     console.log('onCallClick');
+    //     document.getElementById('root')?.click();
+    // }
+    // return (
+    //     <div>
+    //         <p>DispatchEvent</p>
+    //         <button onClick={onCallDispatchEvent}>call dispatchEvent</button>
+    //         <button onClick={onCallClick}>call click</button>
+    //     </div>
+    // )
+
+    const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log('onChangeValue', e.target.value);
     }
-    const onCallClick = () => {
-        console.log('onCallClick');
-        document.getElementById('root')?.click();
+    const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+        console.log('onChangeFiles', e.target.checked);
     }
+    const onChangeFiles = (e: ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+        console.log('onChangeFiles', e.target.files);
+    }
+
     return (
         <div>
-            <p>DispatchEvent</p>
-            <button onClick={onCallDispatchEvent}>call dispatchEvent</button>
-            <button onClick={onCallClick}>call click</button>
+            <p>OnChange</p>
+            <input type="text" onChange={onChangeValue} placeholder="type some text" defaultValue="Hello" />
+            <input type="checkbox" onChange={onChangeChecked} defaultChecked />
+            <input type="file" onChange={onChangeFiles} multiple accept="images/*" />
         </div>
     )
 }
