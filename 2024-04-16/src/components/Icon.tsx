572bd6656e0a0cfb50732a603a04abc23b774beb
양원준 <anyas1398@gmail.com>
2024-04-17 10:30:34 +0900

    .

--- 2024-04-16/src/components/Icon.tsx
+++ 2024-04-16/src/components/Icon.tsx
@@ -1,10 +1,15 @@
-import type {FC, CSSProperties} from 'react';
+import type { FC, CSSProperties } from 'react';
 
 export type IconProps = {
     name: string;
-    style: CSSProperties;
+    style?: CSSProperties;
+    className?: string;
 };
 
-export const Icon: FC<IconProps> = props => {
-    return <span className="material-icons"/>
+export const Icon: FC<IconProps> = ({ name, ...props }) => {
+    return (
+        <span {...props} className="material-icons">
+            {name}
+        </span>
+    )
 }
