eb1c3dda10555828ef3b78d4fb1e32277a29dd4b
양원준 <anyas1398@gmail.com>
2024-04-24 11:36:09 +0900

    Update PaddingTest.tsx

--- ch03_3/src/pages/PaddingTest.tsx
+++ ch03_3/src/pages/PaddingTest.tsx
@@ -1,11 +1,20 @@
 import { Title } from "../components";
 import * as D from "../data";
+import React from "react";
 
+const sentence = D.randomSentence(10);
 export default function CopyMe() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>PaddingTest</Title>
+      <div className="p-8">
+        <div className="text-white bg-sky-600">
+          <p>{sentence}</p>
+        </div>
+        <div className="p-8 text-white bg-orange-600">
+          <p>{sentence}</p>
+        </div>
+      </div>
     </section>
   );
 }
