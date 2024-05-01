4c12f8d71508f8e36f8323e30848df5dfff7c3a9
양원준 <anyas1398@gmail.com>
2024-05-01 10:27:58 +0900

    Update App.tsx

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import Clock from "./pages/Clock";
 
 export default function App() {
   let today = new Date();
+  useEffect(() => {
+    console.log('useEffect called.');
+    const duration = 1000;
+    const id = setInterval(() => {
+      today = new Date();
+      console.log('today: ' + today.toLocaleDateString());
+    }, duration);
+    return () => clearInterval(id);
+  }, [])
   return <Clock today={today} />;
 }
